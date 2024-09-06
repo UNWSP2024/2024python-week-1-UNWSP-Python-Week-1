1a9823ee3af65587c105afdaad2f8e7c3f5ac0be
trevorneutgens <tjneutgens@students.unwsp.edu>
2024-09-06 16:17:20 -0500

    final

--- program_2.py
+++ program_2.py
@@ -1,4 +1,10 @@
 def proverbs_22_29():
+    # Made by Trevor Neutgens on 9/6/24, codename: proverbs 22:29
+    print('''"Do you see a man skillful in his work?''')
+    print(" ")
+    print("He will stand before kings;")
+    print(" ")
+    print('''he will not stand before obscure men."''')
     
 
 proverbs_22_29()
